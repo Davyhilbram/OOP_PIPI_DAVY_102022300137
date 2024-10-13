005dbaa8587fae4d9b563a5ae9c95c4379c2feb9
Davy Hilbram <davyhilbram57@gmail.com>
2024-10-13 16:37:34 +0700

    init: modul 0 commit

---
+++ MODUL 0/DadahDunia.java
@@ -0,0 +1,5 @@
+public class DadahDunia {
+        public static void main(String[] args) {
+            System.out.println("Goodbye, World");
+        }
+    }
---
+++ MODUL 1/Penerbangan.java
@@ -0,0 +1,48 @@
+public class Penerbangan {
+    private String kodePenerbangan;
+    private String bandaraKeberangkatan;
+    private String bandaraTujuan;
+    private String waktuKeberangkatan;
+    private String waktuKedatangan;
+    private float hargaTiket;
+
+    public Penerbangan(String nomorPenerbangan, String bandaraKeberangkatan, String bandaraTujuan, String waktuKeberangkatan, String waktuKedatangan, float hargaTiket) {
+        this.kodePenerbangan = nomorPenerbangan;
+        this.bandaraKeberangkatan = bandaraKeberangkatan;
+        this.bandaraTujuan = bandaraTujuan;
+        this.waktuKeberangkatan = waktuKeberangkatan;
+        this.waktuKedatangan = waktuKedatangan;
+        this.hargaTiket = hargaTiket;
+    }
+
+    public void tampilDaftarPenerbangan() {
+        System.out.println("Nomor Penerbangan: " + kodePenerbangan);
+        System.out.println("Bandara Keberangkatan: " + bandaraKeberangkatan + " ---> Bandara Tujuan: " + bandaraTujuan);
+        System.out.println("Waktu Keberangkatan: " + waktuKeberangkatan + " ---> Waktu Kedatangan: " + waktuKedatangan);
+        System.out.println("Harga Tiket: Rp." + hargaTiket);
+    }
+
+    public String getNomorPenerbangan() {
+        return kodePenerbangan;
+    }
+
+    public String getBandaraKeberangkatan() {
+        return bandaraKeberangkatan;
+    }
+
+    public String getBandaraTujuan() {
+        return bandaraTujuan;
+    }
+
+    public String getWaktuKeberangkatan() {
+        return waktuKeberangkatan;
+    }
+
+    public String getWaktuKedatangan() {
+        return waktuKedatangan;
+    }
+
+    public float getHargaTiket() {
+        return hargaTiket;
+    }
+}
